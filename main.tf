56e64b1a645e92546d458978f8524f49efdaea88
Judy Marcelo <judy.marcelo@gmail.com>
2022-03-16 02:37:45 +0000

    Add second tag in resource group

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ resource "azurerm_resource_group" "myresourcegroup" {
   tags = {
     environment = "Production"
     Billable    = "true"
+    Department = "devops"
   }
 }
 
@@ -109,7 +110,7 @@ resource "azurerm_virtual_machine" "catapp" {
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
   tags = {
-    Department = "devops"
+    
   }
 
 
